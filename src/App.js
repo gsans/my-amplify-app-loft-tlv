c392d8b0802a5cac844adf94d6213bfd37105288
Gerard Sans <gsans@amazon.com>
2019-06-22 22:05:20 +0300

    add form and reset input

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-
-import { Auth } from 'aws-amplify'
+import React, { useEffect, useState, useReducer } from 'react'
 import { API, graphqlOperation } from 'aws-amplify'
+import { withAuthenticator } from 'aws-amplify-react'
 import { listCoins } from './graphql/queries'
+import { createCoin as CreateCoin } from './graphql/mutations'
+import './App.css';
+
+// import uuid to create a unique client ID
+import uuid from 'uuid/v4'
+const CLIENT_ID = uuid()
+
+// create initial state
+const initialState = {
+  name: '', price: '', symbol: '', coins: []
+}
+
+// create reducer to update state
+function reducer(state, action) {
+  switch(action.type) {
+    case 'SETCOINS':
+      return { ...state, coins: action.coins }
+    case 'SETINPUT':
+      return { ...state, [action.key]: action.value }
+    case 'CLEARINPUT':
+        return { ...state, name: '', price: '', symbol: '' }
+    default:
+      return state
+  }
+}
 
 function App() {
-  const [coins, updateCoins] = useState([])
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
-    Auth.currentAuthenticatedUser()
-    .then(user => console.log({ user }))
-    .catch(error => console.log({ error }))
-
     getData()
   }, [])
 
@@ -22,16 +39,63 @@ function App() {
     try {
       const coinData = await API.graphql(graphqlOperation(listCoins))
       console.log('data from API: ', coinData)
-      updateCoins(coinData.data.listCoins.items)
+      dispatch({ type: 'SETCOINS', coins: coinData.data.listCoins.items })
     } catch (err) {
       console.log('error fetching data..', err)
     }
   }
 
+  async function createCoin() {
+    const { name, price, symbol } = state
+    if (name === '' || price === '' || symbol === '') return
+    const coin = {
+      name, price: parseFloat(price), symbol, clientId: CLIENT_ID
+    }
+    const coins = [...state.coins, coin]
+    dispatch({ type: 'SETCOINS', coins })
+    console.log('coin:', coin)
+    
+    try {
+      await API.graphql(graphqlOperation(CreateCoin, { input: coin }))
+      console.log('item created!')
+      dispatch({ type: 'CLEARINPUT' }) //clear input
+    } catch (err) {
+      console.log('error creating coin...', err)
+    }
+  }
+
+  // change state then user types into input
+  function onChange(e) {
+    dispatch({ type: 'SETINPUT', key: e.target.name, value: e.target.value })
+  }
+
+  // add UI with event handlers to manage user input
   return (
-    <div>
+    <div><form autoComplete='off'>
+      <input
+        name='name'
+        placeholder='name'
+        onChange={onChange}
+        value={state.name}
+        autoComplete='off'
+      />
+      <input
+        name='price'
+        placeholder='price'
+        onChange={onChange}
+        value={state.price}
+        autoComplete='off'
+      />
+      <input
+        name='symbol'
+        placeholder='symbol'
+        onChange={onChange}
+        value={state.symbol}
+        autoComplete='off'
+      />
+      <button type="button" onClick={createCoin}>Create Coin</button>
       {
-        coins.map((c, i) => (
+        state.coins.map((c, i) => (
           <div key={i}>
             <h2>{c.name}</h2>
             <h4>{c.symbol}</h4>
@@ -39,7 +103,7 @@ function App() {
           </div>
         ))
       }
-    </div>
+    </form></div>
   )
 }
 
