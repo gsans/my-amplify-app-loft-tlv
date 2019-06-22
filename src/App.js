6ccaf0f090e3cbbe74c1fd1b54bb48a3ff41350c
Gerard Sans <gsans@amazon.com>
2019-06-22 21:17:54 +0300

    Added auth and api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from 'aws-amplify-react'
+
+import { Auth } from 'aws-amplify'
+import { API, graphqlOperation } from 'aws-amplify'
+import { listCoins } from './graphql/queries'
 
 function App() {
+  const [coins, updateCoins] = useState([])
+
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+    .then(user => console.log({ user }))
+    .catch(error => console.log({ error }))
+
+    getData()
+  }, [])
+
+  async function getData() {
+    try {
+      const coinData = await API.graphql(graphqlOperation(listCoins))
+      console.log('data from API: ', coinData)
+      updateCoins(coinData.data.listCoins.items)
+    } catch (err) {
+      console.log('error fetching data..', err)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        coins.map((c, i) => (
+          <div key={i}>
+            <h2>{c.name}</h2>
+            <h4>{c.symbol}</h4>
+            <p>{c.price}</p>
+          </div>
+        ))
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true })
